cb058d128794aa1e2115f0711cc44bacc7773f7e
Kartik saini <kartiksaini@cedcommerce.com>
2022-03-09 17:17:25 +0530

    change date difference

--- src/datediff.php
+++ src/datediff.php
@@ -1,26 +1,18 @@
 <?php
  class datediff{
-     public $date1;
-     public $date2;
-     public function  differ($date1,$date2)
+    
+     public function  differ()
      {
-         $this->date1= $date1;
-         $this->date2= $date2;
-         $sdate =$this->date1;
-         $edate = $this->date2;
+         
      
-         $diff = abs($date2-$date1);
 
-        
-  $years = floor($diff / (365*60*60*24));
- 
-
-  $months = floor(($diff - $years * 365*60*60*24) / (31*60*60*24));
- 
-  $days = floor(($diff - $years * 365*60*60*24 - $months*31*60*60*24) / (60*60*24)-2);
+        $sdate = new DateTime("1981-11-03");
+        $edate = new DateTime("2013-09-04");
+        $interval = $sdate->diff($edate);
+        echo "Difference : " . $interval->y . " years, " . $interval->m." months, ".$interval->d." days ";
 
 
-  echo "Difference:".$years."year, ". $months."months,".$days." days";
+
 
 
       
@@ -34,9 +26,8 @@
 
 
  $obj = new datediff();
- $d1 = strtotime("1981-11-03");
- $d2 = strtotime("2013-09-04");
- $obj->differ($d1,$d2); 
+ 
+ $obj->differ(); 
 
 
 
