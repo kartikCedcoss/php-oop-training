fdf8eb3c6cdfa83b9335aad805893ad7f97b96bc
Kartik saini <kartiksaini@cedcommerce.com>
2022-03-09 17:20:49 +0530

    factorial

--- src/datediff.php
+++ src/datediff.php
@@ -3,30 +3,17 @@
     
      public function  differ()
      {
-         
-     
 
         $sdate = new DateTime("1981-11-03");
         $edate = new DateTime("2013-09-04");
         $interval = $sdate->diff($edate);
         echo "Difference : " . $interval->y . " years, " . $interval->m." months, ".$interval->d." days ";
-
-
-
-
-
-      
-
-
-
-
      }
     
  }
 
 
  $obj = new datediff();
- 
  $obj->differ(); 
 
 
